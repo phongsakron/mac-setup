eaf936e54b84dfa33ccbb45f9fa956bdccf71eb3
Phongsakron Dutsadee <phongsakrondutsadee@gmail.com>
2021-12-03 09:25:18 +0000

    brew install

---
+++ setup.sh
@@ -0,0 +1,40 @@
+mkdir ~/dev
+mkdir ~/pdev
+
+brew update
+# terminal
+brew install --cask iterm2 
+
+# recorder
+brew install blackhole-16ch
+
+# dev
+brew install git
+brew install gradle
+brew install nvm
+brew install --cask google-chrome
+brew install --cask visual-studio-code
+brew install --cask responsively
+brew install --cask postman
+brew install --cask jetbrains-toolbox
+
+# productivity
+brew install --cask rectangle
+brew install --cask jumpcut
+brew install --cask hyperswitch
+brew install --cask notion
+
+# communication
+brew install --cask discord
+
+# appinstaller
+brew install --cask setapp
+# install time-out , cleanshot x , clean my mac
+
+# manual install app
+# line
+
+
+
+nvm install stable
+
